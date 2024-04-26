4ae3493c35424f97eb2b4ea492c89fe9970f4c64
Lscutelx <lscutelx@gmail.com>
2024-04-26 16:51:50 +0300

    Add road

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const canvas = document.getElementById('myCanvas'); // Полученние эл
 canvas.width = 200; // Задаем ширину
 
 const ctx = canvas.getContext('2d'); // Получение контекста рисования
-const car = new Car(100, 100, 30, 50); // Создание нового экземпляра класса машины
-car.draw(ctx); // Рисование машины с использованием контекста рисования
+const road = new Road(canvas.width/2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50); // Создание нового экземпляра класса машины
+
 
 animate();
 
@@ -12,6 +13,13 @@ function animate() {
     car.update(); // Вызываем метод update
 
     canvas.height = window.innerHeight; //Задаем высоту канвасу
-    car.draw(ctx);
+
+    ctx.save();
+    ctx.translate(0, -car.y + canvas.height * 0.7);
+
+    road.draw(ctx);
+    car.draw(ctx); // Рисование машины с использованием контекста рисования
+
+    ctx.restore();
     requestAnimationFrame(animate); // Запускаем animate() много раз в секунду
 }
